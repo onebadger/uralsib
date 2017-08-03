08419e3b6f4513dda7fe0bff79dc64d58997177b
onebadger <yakov.barsukov@gmail.com>
2017-08-03 18:58:59 +0300

    calculator logic planning

---
+++ js/app.js
@@ -0,0 +1,42 @@
+// change and count calc. conditions according to the users input
+
+// test if the user input data matches clients list elements
+  // if the user data matches
+    // show modal window asking if the user wants to accept the bank offer
+      // if user accept the offer
+        // apply conditions to the calculator
+        // hide insurance field below
+        // let user change the sum and the period of credit according to conditions applied
+      // if user doesn't accept the offer
+        // use calculator without conditions
+        // let user change the sum and the period of credit according to conditions applied
+        // change credit percent if insurance on/off
+  // if the user data doesn't match (or there is no data)
+    // use calculator without conditions
+    // let user change the sum and the period of credit according to conditions applied
+    // change credit percent if insurance on/off
+
+
+
+// personal bank offer
+// credit sum 900000 rub.
+// credit sum range 35000 - 1500000 rub.
+// credit period 36 mo.
+// credit period range 12 - 84 mo.
+// credit percent 15% yr.
+
+
+// common bank offer
+// credit sum 900000 rub.
+// credit sum range 50000 - 1000000 rub.
+// credit period 36 mo.
+// credit period range 12 - 60 mo.
+// credit percent 19% yr. with insurance
+// credit percent 22% yr. without insurance
+
+
+
+
+
+
+
