ab3f859d294bfa63b9b85dabf39eb47457c591fa
onebadger <yakov.barsukov@gmail.com>
2017-08-03 18:59:20 +0300

    upload files

---
+++ js/clients.js
@@ -0,0 +1,20 @@
+var clientsList = [
+  {
+    firstName : 'ИВАНОВ',
+    secondName : 'ИВАН',
+    middleName : 'ИВАНОВИЧ',
+    birthDate : '01.01.1971'
+  },
+  {
+    firstName : 'ПЕТРОВ',
+    secondName : 'ПЕТР',
+    middleName : 'ПЕТРОВИЧ',
+    birthDate : '02.02.1972'
+  },
+  {
+    firstName : 'НИКОЛАЕВ',
+    secondName : 'НИКОЛАЙ',
+    middleName : 'НИКОЛАЕВИЧ',
+    birthDate : '03.03.1973'
+  }
+]
